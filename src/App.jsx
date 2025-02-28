ec30aa9cf6ab93532013c7f4e223079618c18f80
sunabunacodes <ysunamawit@gmail.com>
2025-02-28 16:04:56 -0500

    removed comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import * as React from 'react'; 
+import * as React from 'react';
 
-const App = () => { // React Function Component
-  const stories = [ // Lists in React
+const App = () => {
+  const stories = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
@@ -20,52 +20,32 @@ const App = () => { // React Function Component
     },
   ];
 
-  const handleSearch = (event) => { // Handler Function in JSX
-    console.log(event.target.value); // [4: Callback Handlers in JSX]: handleSearch logs search term
+  const handleSearch = (event) => {
+    console.log(event.target.value);
   };
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-
-      {/* This is an instance of the Search component, so we say it's an element */}
-      <Search onSearch={handleSearch} /> {/* React Props */}
-
+      <Search onSearch={handleSearch} />
       <hr />
-
-      <List list={stories} /> {/* React Props */} {/* [5: Callback Handlers in JSX]: App passes stories to List */}
-
+      <List list={stories} />
     </div>
   );
 };
 
-/*
-React Component Overview
-- Root components are always at the top of a component tree
-- Leaf components do not render other components
-- Components can have zero, one, or many child components
-- Refactoring involves extracting components
-*/
-const Search = (props) => { 
-  /*
-  React State
-  - stateful value that changes over time
-  - React.useState is called a react hook
-  - state or prop changes cause a re-render
-  */
+const Search = (props) => {
   const [searchTerm, setSearchTerm] = React.useState('');
 
-  const handleChange = (event) => { // Callback Handlers in JSX
-    setSearchTerm(event.target.value); // [2: Callback Handlers in JSX]: handleChange updates searchTerm
-
-    props.onSearch(event); // React Props // [3: Callback Handlers in JSX]: handleChange calls onSearch
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    props.onSearch(event);
   };
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} /> {/* [1: Callback Handlers in JSX]: "Testing" in search input */}
-
+      <input id="search" type="text" onChange={handleChange} />
       <p>
         Searching for <strong>{searchTerm}</strong>.
       </p>
@@ -73,15 +53,15 @@ const Search = (props) => {
   );
 };
 
-const List = (props) => ( 
+const List = (props) => (
   <ul>
-    {props.list.map((item) => ( // Lists in React
-      <Item key={item.objectID} item={item} /> // React Props
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
     ))}
   </ul>
 );
 
-const Item = (props) => ( 
+const Item = (props) => (
   <li>
     <span>
       <a href={props.item.url}>{props.item.title}</a>
@@ -92,4 +72,4 @@ const Item = (props) => (
   </li>
 );
 
-export default App; 
+export default App;
